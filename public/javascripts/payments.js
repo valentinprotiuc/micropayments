48fafa10d2bfcc8302be578f82483ebd540e5f50
Valentin Protiuc <v_prot01@uni-muenster.de>
2018-08-10 13:18:06 +0200

    Update payments.js

--- public/javascripts/payments.js
+++ public/javascripts/payments.js
@@ -99,7 +99,7 @@ window.onload = function () {
     var options = {
         horizon: 'live',
         address: 'GAFA6RZDVDNTO77W3DQGXTFQCJ4FRDXO5QX23ADT4JONXE2IYA5V5EID',
-        currency: 'EUR',
+        currency: 'USD',
         onConfirm: myOrderConfirm,
         onCancel: myOrderCancel
     };
