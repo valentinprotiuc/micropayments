544e572fbb9990fcf4768add5eb04239facdbfd7
Valentin Protiuc <v_prot01@uni-muenster.de>
2018-08-10 13:21:00 +0200

    Update ipn.js

--- routes/ipn.js
+++ routes/ipn.js
@@ -19,7 +19,7 @@ router.get('/:address-:price', function (req, res) {
 
     var searchValues = {'addresses': address};
 
-    req.on('abort', function (){
+    req.on('aborted', function (){
         res.end;
     });
 
